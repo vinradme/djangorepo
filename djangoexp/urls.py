90edfe36b2450b5954c2dc7a437f6a4cc8361857
Vinodhini Ravikumar <vinravik@cisco.com>
2015-04-13 17:28:35 -0700

    Second Commit! Added templates and static files

---
+++ djangoexp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+import core.views as coreviews
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'djangoexp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', coreviews.SplashView.as_view()),
+
+]
